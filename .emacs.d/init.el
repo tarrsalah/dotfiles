87bca402020ace347d5a9bdc0caf6e609ebba8ab
tarrsalah <tarrsalah@gmail.com>
2020-07-07 22:30:38 +0100

    emcas: disable electric indent mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,7 +11,7 @@
 (global-auto-revert-mode)
 (set-window-margins nil 2)
 (electric-pair-mode 1)
-(electric-indent-mode 1)
+(electric-indent-mode 0)
 (global-linum-mode t)
 (fset 'yes-or-no-p 'y-or-n-p)
 (setq shift-select-mode t)
