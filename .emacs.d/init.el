2c2aad13bb6b803ff0ae2a6f7265e74a9cb529e4
Salah Eddine Taouririt <tarrsalah@gmail.com>
2020-08-31 12:22:35 +0100

    emacs: js2 spacing.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -219,6 +219,7 @@
   :ensure t
   :init
   (progn
+    (add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2)))
     (setq js2-mode-show-parse-errors nil)
     (setq js2-mode-show-strict-warnings nil)
     (setq js2-strict-missing-semi-warning nil)
