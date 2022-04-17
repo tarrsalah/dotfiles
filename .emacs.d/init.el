90b2189d31f8e5fa8a2015d4758bc0e078d6ffb0
Salah Eddine Taouririt <tarrsalah@gmail.com>
2022-04-17 20:24:38 +0200

    emacs: update key binding for autocomplete.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -152,7 +152,7 @@
     (setq company-idle-delay 1)
     (setq company-echo-delay 0)
     (setq company-begin-commands '(self-insert-command)))
-  :bind (("C-n" . company-complete)))
+  :bind (("M-n" . company-complete)))
 
 ;; projectile
 (use-package projectile
